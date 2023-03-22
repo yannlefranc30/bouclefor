3e1c3ff52e235760d85fdd0a2f876983bf8070b4
yann <yannlefranc330@gmail.com>
2023-03-22 13:55:59 +0100

    deuxieme boucle

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+//olusieur fois sur un méme bloc de code}
+
+public class Main {
+
+    // Un commentaire pour donner l'exemple
+    //olusieur fois sur un méme bloc de code
+    public static void main(String[] args) {
+
+        // declaration de la boucle for
+
+        for (int i = 0; i < 5; i++) {
+
+            if (i == 8) {
+                break;
+            }
+
+            //permet d'afficher par println les chiffres
+            System.out.println(i);
+        }
+    }
+}
